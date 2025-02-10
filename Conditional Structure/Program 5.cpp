f74cac9ad3db8e72c9aca8bd3a37ec7a22742d41
Emaan2006 <emaanfatima4612@gmail.com>
2025-02-11 00:23:02 +0500

    Add files via upload

---
+++ Conditional Structure/Program 5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num, reverse = 0;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    while (num != 0) {
+        reverse = reverse * 10 + (num % 10);  
+        num /= 10; 
+    }
+    cout << "Reversed number: " << reverse << endl;
+
+    return 0;
+}
